66f30cdea6bbdc2dd270bac0261747ef1393c08d
Genie <60766070+srubaiyat05@users.noreply.github.com>
2020-06-12 16:57:23 -0700

    Create AssignmentCategory.java

---
+++ AssignmentCategory.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+public class AssignmentCategory
+{
+    public String name;
+    public double weight;
+    
+    public AssignmentCategory(String n, double w)
+    {
+        name = n;
+        weight = w;
+    }
+}
+
